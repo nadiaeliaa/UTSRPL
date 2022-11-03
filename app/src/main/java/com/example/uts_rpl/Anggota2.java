c8fa7c9e74b690a843f68dcd269c5b18c08ce508
Septian Adi Pamungkas <92453593+SeptianAdiPamungkas33@users.noreply.github.com>
2022-11-03 20:14:14 +0700

    Update Anggota2.java

--- app/src/main/java/com/example/uts_rpl/Anggota2.java
+++ app/src/main/java/com/example/uts_rpl/Anggota2.java
@@ -9,6 +9,6 @@ public class Anggota2 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_anggota1);
+        setContentView(R.layout.activity_anggota2);
     }
 }
